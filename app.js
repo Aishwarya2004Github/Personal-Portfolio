1ad84561c2bdec90766fe28776d589ce6ec25c9f
Aishwarya Rana <160381307+Aishwarya2004Github@users.noreply.github.com>
2024-10-12 22:13:49 +0530

    commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config(); // Load environment variables
 const app = express();
 
 // Connect to MongoDB Atlas
-console.log('MONGODB_URI:', process.env.MONGODB_URI); 
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -31,66 +31,32 @@ app.use(bodyParser.json());
 
 // Serve HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Handle form submission
-app.post('/details', (req, res, next) => {
+app.post('/details', async (req, res, next) => {
   const { Name, email, message } = req.body;
 
   // Create a new message instance
   const newMessage = new Message({ Name, email, message });
 
-  // Save the message to the database
-  newMessage.save((err) => {
-    if (err) {
-      console.error('Error saving data to the database:', err);
-      next(err);
-      return;
-    }
-
+  try {
+    // Save the message to the database
+    await newMessage.save();
     console.log('Data saved to the database:', newMessage);
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Form Submission Success</title>
-          <style>
-            body {
-              font-family: Arial, sans-serif;
-              background-color: black;
-              text-align: center;
-              margin: 20% auto;
-              max-width: 600px;
-              padding: 20px;
-              border-radius: 10px;
-              box-shadow: 0 0 10px #04fc43;
-            }
-            h1 {
-              color: #d13639;
-            }
-            p {
-              color: #ff04f;
-            }
-            a {
-              color: #3498db;
-              text-decoration: none;
-            }
-            a:hover {
-              text-decoration: underline;
-            }
-          </style>
-      </head>
-      <body>
-          <h1>Form Submitted Successfully</h1>
-          <p>Your form has been submitted successfully.</p>
-          <p><a href="/">Go back to the form</a></p>
-      </body>
-      </html>
-    `);
-  });
+
+    // Redirect to details.html upon successful submission
+    res.redirect('/details');
+  } catch (err) {
+    console.error('Error saving data to the database:', err);
+    next(err); // Call the error handling middleware
+  }
+});
+
+// Serve the details page
+app.get('/details', (req, res) => {
+  res.sendFile(path.join(__dirname, 'details.html'));
 });
 
 // Serve static files
